9ff20889eeac8b0f3d9a820337568adcff77515b
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-02-05 15:25:09 +0700

    Update and rename gen_1.js to gen_2.js

--- gg/gen_2.js
+++ gg/gen_2.js
@@ -1,4 +1,4 @@
-l3file("https://cdn.statically.io/gh/hamdaniazzah/a/main/gg/css/gen_1.min.css", "css")
+l3file("https://cdn.statically.io/gh/hamdaniazzah/a/main/gg/css/gen_2.min.css", "css")
 l3file("https://cdn.ampproject.org/v0.js", "js")
 l3file("https://cdn.statically.io/gh/hamdaniazzah/a/main/gg/js/gen_1.min.js", "js")
 l3file("https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.2.2/lazysizes.min.js", "js")

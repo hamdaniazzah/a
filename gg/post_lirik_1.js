9034955ae0697d94dafc4e591b5da5d7a0fbd5d3
Hamdani <53028113+hamdaniazzah@users.noreply.github.com>
2021-02-05 12:18:11 +0700

    Update and rename post_lirik_0.js to post_lirik_1.js

---
+++ gg/post_lirik_1.js
@@ -0,0 +1,3 @@
+l3file("https://cdn.statically.io/gh/hamdaniazzah/a/main/gg/css/post_lirik_1.min.css", "css")
+l3file("https://cdn.statically.io/gh/hamdaniazzah/a/main/gg/js/post_lirik_1.min.js", "js")
+l3file("https://cdn.ampproject.org/v0/amp-bind-0.1.js", "js")
